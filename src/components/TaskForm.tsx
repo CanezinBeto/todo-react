96eeb736c548c642bafd783f7ef9395cf5d3b712
CanezinBeto <albertocanezin@gmail.com>
2022-06-17 10:09:48 -0300

    Finalizando atualização de tarefas

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -8,9 +8,16 @@ interface Props {
   taskList: ITask[]
   setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>>
   task?: ITask | null
+  handleUpdate?(id: number, title: string, difficulty: number): void
 }
 
-const TaskForm = ({ btnText, taskList, setTaskList, task }: Props) => {
+const TaskForm = ({
+  btnText,
+  taskList,
+  setTaskList,
+  task,
+  handleUpdate,
+}: Props) => {
   const [id, setId] = useState<number>(0)
   const [title, setTitle] = useState<string>('')
   const [difficulty, setDifficuty] = useState<number>(0)
@@ -25,12 +32,17 @@ const TaskForm = ({ btnText, taskList, setTaskList, task }: Props) => {
 
   const addTaskHandler = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    const id: number = Math.floor(Math.random() * 1000)
-    const newTask: ITask = { id, title, difficulty }
 
-    setTaskList!([...taskList, newTask])
-    setTitle('')
-    setDifficuty(0)
+    if (handleUpdate) {
+      handleUpdate(id, title, difficulty)
+    } else {
+      const id: number = Math.floor(Math.random() * 1000)
+      const newTask: ITask = { id, title, difficulty }
+
+      setTaskList!([...taskList, newTask])
+      setTitle('')
+      setDifficuty(0)
+    }
   }
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
